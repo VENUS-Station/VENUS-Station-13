cf364fc9397f6fbdb655d64f03321647df3b637f
ariaworld <143797359+ariaworld@users.noreply.github.com>
2026-01-05 19:26:30 +0100

    [PORT] Nova underworld connections quirk

---
+++ modular_zzvenus/code/modules/underworld_connections/black_market_uplink.dm
@@ -0,0 +1,3 @@
+// NOVA PORT - Make black market uplink small
+/obj/item/market_uplink
+	w_class = WEIGHT_CLASS_SMALL
---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/venus/underworld_connections.tsx
@@ -0,0 +1,23 @@
+// NOVA PORT
+import {
+  type Feature,
+  type FeatureChoiced,
+  FeatureShortTextInput,
+  FeatureTextInput,
+} from '../../base';
+import { FeatureDropdownInput } from '../../dropdowns';
+
+export const underworld_uplink_skin: FeatureChoiced = {
+  name: 'Uplink Skin',
+  component: FeatureDropdownInput,
+};
+
+export const underworld_uplink_name: Feature<string> = {
+  name: 'Uplink Name',
+  component: FeatureShortTextInput,
+};
+
+export const underworld_uplink_desc: Feature<string> = {
+  name: 'Uplink Description',
+  component: FeatureTextInput,
+};
